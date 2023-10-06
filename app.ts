eaffe626edd33f9787a0fe717d3a6b6eda4a07ee
alex <alex@nanoinsure.com>
2023-10-06 18:10:03 +0800

    将app.js换成app.ts

--- app.ts
+++ app.ts
@@ -1,3 +1,5 @@
+import { ErrorRequestHandler, RequestHandler } from 'express';
+
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
@@ -15,7 +17,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,19 +25,18 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+    next(createError(404));
+} as RequestHandler);
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+} as ErrorRequestHandler);
 module.exports = app;
